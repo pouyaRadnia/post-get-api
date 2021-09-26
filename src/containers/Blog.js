24aef9fb57f2ac5db4a096c6f8a162b311870977
pouyaRadniya <pouyaradniya8@gmail.com>
2021-09-26 12:52:08 +0330

    import Post,NewPost,FullPost in Blog

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Post from '../components/Post/Post'
+import NewPost from '../components/NewPost/NewPost'
+import FullPost from '../components/FullPost/FullPost'
 import './Blog.css'
 
  class Blog extends React.Component {
@@ -6,13 +9,14 @@ import './Blog.css'
          return(
              <div>
                  <section>
-
+                    <Post />
+                    <Post />
                  </section>
                  <section>
-
+                    <FullPost />
                  </section>
                  <section>
-
+                    <NewPost />
                  </section>
              </div>
          )
