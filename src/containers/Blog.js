3cb74536609554d2bfddde67b3dffa873a36431b
pouyaRadniya <pouyaradniya8@gmail.com>
2021-09-26 11:26:29 +0330

    app layout

---
+++ src/containers/Blog.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Blog.css'
+
+ class Blog extends React.Component {
+     render () {
+         return(
+             <div>
+                 <section>
+
+                 </section>
+                 <section>
+
+                 </section>
+                 <section>
+
+                 </section>
+             </div>
+         )
+     }
+ }
+ export default Blog
