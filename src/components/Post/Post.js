a1e5511e3345bd3a6d39482596cc24f63cf2eba1
pouyaRadniya <pouyaradniya8@gmail.com>
2021-09-26 12:48:38 +0330

    create Post structure

---
+++ src/components/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'Post.css'
+
+const Post = (props) =>{
+    <article>
+        <h1>title</h1>
+        <div>
+            <div className='author'>Author</div>
+        </div>
+    </article>
+}
+export default Post
